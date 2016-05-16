0a17ec503bb794d241d9eba5c2fb24caa7243e49
Dmitry Kovpak <Dmitry Kovpak@DESKTOP-CA3K60L>
2016-05-16 10:11:53 +0300

    Добавьте файлы проекта.

---
+++ EmsysAutoTests/LoginPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmsysAutoTests
+{
+   public  class LoginPage
+    {
+        public static void GoTo()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https//qa.emsys1.com");
+        }
+
+        public static LoginCommand LoginAs(string userName)
+        {
+            
+        }
+    }
+
+    public class LoginCommand
+    {
+
+    }
+}
---
+++ EmsysTests/CanLoginAsDkovpak.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmsysAutoTests;
+
+namespace EmsysTests
+{
+    [TestClass]
+    public class CanLoginAsDkovpak
+    {
+        [TestMethod]
+        public void LoginAsDkovpak()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("admin").WithPassword("password").Login();
+            Assert.IsTrue(DashboardPage.IsAt, "Failed to login.");
+        }
+    }
+}
